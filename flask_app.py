6c76e6adc5607946bc4684cb8f511309d4ad47c1
shartrich <snhartrich@gmail.com>
2018-05-12 17:51:27 +0000

    added db stuff

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,38 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+
+#import pymysql
+# sql_user = 'shartrich'
+# sql_pwd = 'MySQL123!'
+# sql_db = 'shartrich$flask_app'
+# sql_host = 'shartrich.mysql.pythonanywhere-services.com'
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="shartrich",
+    password="MySQL123!",
+    hostname='shartrich.mysql.pythonanywhere-services.com',
+    databasename="shartrich$flask_app_db",
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 comments = []
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -13,4 +41,7 @@ def index():
 
     comments.append(request.form["contents"])
     return redirect(url_for('index'))
+
+
+
 
