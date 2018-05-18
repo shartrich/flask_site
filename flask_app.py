f31360bcd698b0ee928a402c4f302f99e400e6bc
Sean <snhartrich@gmail.com>
2018-05-18 00:16:37 -0500

    .
    .

--- flask_app.py
+++ flask_app.py
@@ -46,8 +46,8 @@ experience_header = {'Home': ['', '/aboutMe'], 'Past Experience': ['active', '/p
 skills_header = {'Home': ['', '/aboutMe'], 'Past Experience': ['', '/pastExp'], 'Skills': ['active', '/skills']}
 
 side_bar_index = {'About': ["#one", "active"], 'Python Libraries': ['#two', ''], 'Software': ['#three', ''], 'Extras': ['#four', '']}
-side_base_past_exp = {'Data Analytics Specialist': ["#one", "active"], 'Operations Data Analyst': ['#two', ''], 'Operations Data Analyst': ['#three', ''], 'Past Academic Projects': ['#four', '']}
-side_base_skills = {'Coding': ["#one", "active"], 'Education': ['#two', ''], 'Skills': ['#three', '']}
+side_bar_past_exp = {'Data Analytics Specialist': ["#one", "active"], 'Operations Data Analyst': ['#two', ''], 'Operations Data Analyst': ['#three', ''], 'Past Academic Projects': ['#four', '']}
+side_bar_skills = {'Coding': ["#one", "active"], 'Education': ['#two', ''], 'Skills': ['#three', '']}
 
 
 
@@ -57,18 +57,15 @@ def index_hello():
 
 @app.route("/aboutMe")
 def me_page():
-    index_header
-    return render_template('index.html', header_info = header_diretions, side_bar = side_bar_index)
+    return render_template('index.html', header_info = index_header, side_bar = side_bar_index)
 
 @app.route("/pastExp")
 def exp_page():
-    experience_header
-    return render_template('pastExp.html', header_info = header_diretions, side_bar = side_base_past_exp)
+    return render_template('pastExp.html', header_info = experience_header, side_bar = side_bar_past_exp)
 
 @app.route("/skills")
 def skills_page():
-    skills_header
-    return render_template('skills.html', header_info = header_diretions, side_bar = side_base_skills)
+    return render_template('skills.html', header_info = skills_header, side_bar = side_bar_skills)
 
 
 
